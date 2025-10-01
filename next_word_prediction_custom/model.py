b5361f59daa1b7a67e18f65f73e72fd31bcaa68e
MaxwellFung <maxwellfung1@gmail.com>
2025-10-01 14:45:55 -0700

    updated to modern library

--- next_word_prediction_custom/model.py
+++ next_word_prediction_custom/model.py
@@ -1,43 +1,28 @@
-# Import required libraries
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
-from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 class GPT2:
     def __init__(self):
-        super(GPT2, self).__init__()
-
-        self.model_type = "GPT2"
-
-        # Load pre-trained model tokenizer (vocabulary)
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
-        # Load pre-trained model (weights)
-        self.model = GPT2LMHeadModel.from_pretrained("gpt2")
-
-    def predict_next(self, text, k):
-        # Encode a text inputs
-        indexed_tokens = self.tokenizer.encode(text)
-
-        # Convert indexed tokens in a PyTorch tensor
-        tokens_tensor = torch.tensor([indexed_tokens])
-
-        # Set the model in evaluation mode to deactivate the DropOut modules
+        self.model = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
         self.model.eval()
 
-        # If you have a GPU, put everything on cuda
-        tokens_tensor = tokens_tensor.to(device)
-        self.model.to(device)
+    def predict_next(self, text, k):
+        # Encode
+        inputs = self.tokenizer.encode(text, return_tensors="pt").to(device)
 
-        # Predict all tokens
+        # Forward pass
         with torch.no_grad():
-            outputs = self.model(tokens_tensor)
-            predictions = outputs[0]
+            outputs = self.model(inputs)
+            logits = outputs.logits  # modern API
 
-        # Get the predicted next sub-word
-        probs = predictions[0, -1, :]
-        top_next = [self.tokenizer.decode(i.item()).strip() for i in probs.topk(k)[1]]
+        # Get probs
+        probs = logits[0, -1, :].softmax(dim=-1)
+        topk = torch.topk(probs, k)
 
-        return top_next
+        words = [self.tokenizer.decode([idx]) for idx in topk.indices]
+        probs = topk.values.tolist()
+
+        return words, probs
